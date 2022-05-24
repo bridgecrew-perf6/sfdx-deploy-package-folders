ddad967725139ce23db6c8e4caa8f6fefc8d847f
Phil Spenceley <phil.spenceley@countdown.co.nz>
2022-05-25 09:42:57 +1200

    first build

---
+++ src/commands/package/directory/deploy.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as os from 'os';
+import * as fs from 'fs';
+import * as child from 'child_process';
+import * as util from 'util';
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages, SfdxProject } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+
+const exec = util.promisify(child.exec);
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('sfdx-deploy-package-folders', 'deploy');
+
+export default class Org extends SfdxCommand {
+  public static description = messages.getMessage('commandDescription');
+
+  public static examples = messages.getMessage('examples').split(os.EOL);
+
+  public static args = [{ name: 'file' }];
+
+  protected static flagsConfig = {
+    // flag with a value (-p, --project=VALUE)
+    project: flags.string({
+      char: 'p',
+      description: messages.getMessage('projectFlagDescription'),
+      default: 'sfdx-project.json',
+    }),
+    options: flags.string({
+      char: 'o',
+      description: messages.getMessage('optionsFlagDescription'),
+      default: '',
+    }),
+  };
+
+  // Comment this out if your command does not require an org username
+  protected static requiresUsername = true;
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = true;
+
+  public async run(): Promise<AnyJson> {
+    const project = await SfdxProject.resolve();
+    const projectJson = await project.resolveProjectConfig();
+    const packageDirectories = projectJson['packageDirectories']; // tslint:disable-line:no-any
+    const options: string = fs.readFileSync(this.flags.options, { encoding: 'utf-8' });
+    const username: string = fs.readFileSync(this.flags.username, { encoding: 'utf-8' });
+
+    for (const packageConfig of packageDirectories as string[]) {
+      await exec('sfdx force:source:deploy -u ' + username + ' ' + options + ' --sourcepath ' + packageConfig);
+    }
+    return;
+  }
+}
